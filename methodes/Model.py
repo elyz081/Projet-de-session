55d507562c26e54f603ecf9e840b29b80defb67f
elyz081 <elyz0801@usherbrooke.ca>
2022-12-12 15:53:21 -0500

    Add score before/after hyperparameters search

--- methodes/Model.py
+++ methodes/Model.py
@@ -5,6 +5,9 @@ class Model:
     def __init__(self, model,name):
         self.model = model
         self.name = name
+        self.best_model=0
+        self.score_before=0
+        self.score_after=0
 
 
     def train(self, X_train, Y_train):
@@ -13,46 +16,40 @@ class Model:
     def predict(self, X):
         return self.model.predict(X)
 
-    def score(self, pred, actual):
-        return metrics.accuracy_score(actual, pred)
 
     def tuning(self,param_grid,X_train, y_train):
 
-        print("# Réglage des hyper-parametres pour %s" %self.name)
-
+        print("# Searching for hyper-parameters for %s" %self.name)
         clf = GridSearchCV(self.model,param_grid, cv=5,scoring='accuracy')
         clf.fit(X_train, y_train)
-        print("Grille des scores :")
+
+        print("Score Grid:")
         score = clf.cv_results_['mean_test_score']
         for test_score, params in zip(score, clf.cv_results_['params']):
-                print("For %r : /n The accuracy of Validation Set %0.3f " % (params,test_score))
+                print("For %r : /n The accuracy is %0.3f " % (params,test_score))
         
-        print("-------------Meilleurs hyper parametres trouvés :------------")
+        print("-------------Best hyper parameters :------------")
         print(clf.best_params_)
-        self.model = clf
+        self.best_model = clf
+        self.score_after=clf.best_score_
 
     
     def crossed_validation(self, X_train, y_train):   
-        #Création de plusieurs divisons de l'ensemble des donnees     
-            print(" --- Validation croisee --- ")
-            sss = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=21) #n_spilt divisions, 0.2 = test 0.8 = train
+  
+            print("------------Crossed Validation :--------------- ")
+            sss = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=21) 
             i=1
             meanScoreCV = 0
             for train_index, test_index in sss.split(X_train, y_train): 
-                print("Iteration ",i)
                 i += 1
                 X_train_fold, X_test_fold = X_train.values[train_index], X_train.values[test_index]
                 y_train_fold, y_test_fold = y_train[train_index], y_train[test_index]
-
-                #Prediction pour chaque partie
+                self.train(X_train_fold,y_train_fold)
                 y_pred_fold = self.predict(X_test_fold)
-
-                #Récupération du score
                 score = accuracy_score(y_test_fold,y_pred_fold)
                 meanScoreCV += score
-                print("Accuracy of the %d eme iteration:" %i,score)
+                print("Accuracy of the %dth iteration:" %i,score)
 
-            #Score moyen pour toutes les divisions
             meanScoreCV /= i-1
-            print("La validation croisée sans recherche d'hyperparametre affiche un score moyen de test  de %.2f" %(meanScoreCV))
-            return meanScoreCV
+            print("The cross-validation of the basic model shows an average score of %.2f" %(meanScoreCV))
+            self.score_before=meanScoreCV
--- methodes/RegressionLogistique.py
+++ methodes/RegressionLogistique.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LogisticRegression
 
 class RegressionLogistique(Model):
     def __init__(self):
-        super().__init__(LogisticRegression(),"Regression Logistique")
+        super().__init__(LogisticRegression(),"Logistic Regression")
         self.params_grid={'solver':["lbfgs","newton-cg"],
                             'C': [0.5,1,5,10,15,20],
                             'max_iter':[100]}
