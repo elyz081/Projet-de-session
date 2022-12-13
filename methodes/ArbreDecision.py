058f7e29131cddd06fbad2c25cc6dc18cd7e9842
elyz081 <elyz0801@usherbrooke.ca>
2022-12-12 19:49:20 -0500

    Edit model parameters

--- methodes/ArbreDecision.py
+++ methodes/ArbreDecision.py
@@ -5,5 +5,5 @@ from methodes.Model import Model
 class ArbreDecision(Model):
     def __init__(self):
         super().__init__(DecisionTreeClassifier(), "Decision Tree")
-        self.params_grid = {'criterion': ["entropy", "gini","log_loss"],
+        self.params_grid = {'criterion': ["entropy", "gini"],
                             'max_depth': [2, 3, 4, 5, 6, 10]}
